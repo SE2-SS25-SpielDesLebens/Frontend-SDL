625c3ef1c8f9daf85e0d3eb08a325549cd8e3f28
Dein GitHub Benutzername <andre.salzer@gmail.com>
2025-03-30 17:56:34 +0200

    Initial App Config

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/MyStomp.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/MyStomp.kt
@@ -14,7 +14,8 @@ import org.hildan.krossbow.stomp.subscribeText
 import org.hildan.krossbow.websocket.okhttp.OkHttpWebSocketClient
 import org.json.JSONObject
 
-private  val WEBSOCKET_URI = "ws://10.0.2.2:8080/websocket-example-broker";
+
+private  val WEBSOCKET_URI = "ws://10.0.2.2:8080/ws";
 class MyStomp(val callbacks: Callbacks) {
 
     private lateinit var topicFlow: Flow<String>
@@ -27,48 +28,57 @@ class MyStomp(val callbacks: Callbacks) {
     private lateinit var session: StompSession
 
     private val scope:CoroutineScope=CoroutineScope(Dispatchers.IO)
+    private var connected = false
+
     fun connect() {
-
-            client = StompClient(OkHttpWebSocketClient()) // other config can be passed in here
-            scope.launch {
-                session=client.connect(WEBSOCKET_URI)
-                topicFlow= session.subscribeText("/topic/hello-response")
-                //connect to topic
-                collector=scope.launch { topicFlow.collect{
-                        msg->
-                    //todo logic
-                    callback(msg)
-                } }
-
-                //connect to topic
-                jsonFlow= session.subscribeText("/topic/rcv-object")
-                jsonCollector=scope.launch { jsonFlow.collect{
-                        msg->
-                    var o=JSONObject(msg)
-                    callback(o.get("text").toString())
-                } }
+        client = StompClient(OkHttpWebSocketClient())
+        scope.launch {
+            try {
+                session = client.connect(WEBSOCKET_URI)
+                // Abonnements einrichten
+                topicFlow = session.subscribeText("/topic/hello-response")
+                collector = scope.launch {
+                    topicFlow.collect { msg ->
+                        callback(msg)
+                    }
+                }
+                jsonFlow = session.subscribeText("/topic/rcv-object")
+                jsonCollector = scope.launch {
+                    jsonFlow.collect { msg ->
+                        val o = JSONObject(msg)
+                        callback(o.get("text").toString())
+                    }
+                }
+                connected = true  // Nur hier setzen, wenn die Verbindung erfolgreich ist
                 callback("connected")
+            } catch (e: Exception) {
+                Log.e("MyStomp", "Error connecting: ${e.localizedMessage}")
+                callback("Error connecting: ${e.localizedMessage}")
             }
-
+        }
     }
     private fun callback(msg:String){
         Handler(Looper.getMainLooper()).post{
             callbacks.onResponse(msg)
         }
     }
-    fun sendHello(){
-
+    fun sendHello() {
         scope.launch {
-            Log.e("tag","connecting to topic")
-
-            session.sendText("/app/hello","message from client")
-           }
+            if (connected) {  // Nur senden, wenn die Session initialisiert wurde
+                Log.e("tag", "sending hello")
+                session.sendText("/app/hello", "message from client")
+            } else {
+                Log.e("tag", "session not connected yet!")
+                callback("session not connected yet!")
+            }
+        }
     }
+
     fun sendJson(){
-        var json=JSONObject();
+        val json=JSONObject();
         json.put("from","client")
         json.put("text","from client")
-        var o=json.toString()
+        val o=json.toString()
 
         scope.launch {
             session.sendText("/app/object",o);
