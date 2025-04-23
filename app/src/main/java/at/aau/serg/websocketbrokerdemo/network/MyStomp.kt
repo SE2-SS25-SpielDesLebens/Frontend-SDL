fbbba651f63f20dd7053ced12cc66c596e010b60
andre <andre.salzer@gmail.com>
2025-04-23 15:57:08 +0200

    Merge conflict

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/network/MyStomp.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/network/MyStomp.kt
@@ -16,6 +16,7 @@ import org.hildan.krossbow.stomp.subscribeText
 import org.hildan.krossbow.websocket.okhttp.OkHttpWebSocketClient
 
 private const val WEBSOCKET_URI = "ws://10.0.2.2:8080/websocket-broker/websocket" // Für Emulator! – anpassen bei echtem Gerät
+//private const val WEBSOCKET_URI = "ws://se2-demo.aau.at:53217/websocket-broker/websocket"
 
 class MyStomp(private val callbacks: Callbacks) {
 
